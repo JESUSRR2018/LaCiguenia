f0882c2f138b1a45c8e89b25c7c69e2efbd82056
APRENDIZ <aprendiz@IBAAPRRTLFSM010.local>
2023-07-19 10:27:43 -0500

    SENA-007 Page navigation bar - Bugs - II - Part I

--- src/app/module/admin/admin-page/components/section-one/section-one.component.ts
+++ src/app/module/admin/admin-page/components/section-one/section-one.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Imagen from 'src/app/shared/imagen/Imagen';
 
 @Component({
   selector: 'app-section-one',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./section-one.component.scss']
 })
 export class SectionOneComponent {
-
+  imgVenta = Imagen.IMG_VENTA_DIA;
+  imgFactura = Imagen.IMG_FACTURA_DIA;
+  imgGastos = Imagen.IMG_GASTOS_DIA;
+  imgUtilidad = Imagen.IMG_UTILIDAD_DIA;
 }
