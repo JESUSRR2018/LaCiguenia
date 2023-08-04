b3336a41ed084cd1fb83fbcee68f2f09824f8bd4
EstebanVera3741 <estebanvera3741@gmail.com>
2023-08-04 11:00:42 -0500

    SENA-031 Section Products to Sell

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login-laciguenia', pathMatch: 'full' },
-  { path: 'login-laciguenia', loadChildren: () => import('./module/login/login.module').then(m => m.LoginModule)},
+  {path: '', redirectTo: 'login-laciguenia', pathMatch: 'full' },
+  {path: 'login-laciguenia', loadChildren: () => import('./module/login/login.module').then(m => m.LoginModule)},
   {path: 'login-laciguenia/admin-page-principal', loadChildren: () => import('./module/admin/admin.module').then(m => m.AdminModule)},
   {path: 'login-laciguenia/caja-page-principal', loadChildren: () => import('./module/caja/caja.module').then(m => m.CajaModule)},
   {path: 'login-laciguenia/pago-page-principal', loadChildren: () => import('./module/pago/pago.module').then(m => m.PagoModule)}
